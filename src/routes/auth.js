08aa464081d83b1998c0f35d4e6141b230a29b3d
RonLionZ <ronaleneng@gmail.com>
2026-01-05 15:11:02 +0100

    Updated Profile Route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,12 +4,23 @@ const { login } = require('../controllers/authController');
 const { verifyToken } = require('../middleware/auth');
 
 router.post('/login', login);
+router.get('/profile', verifyToken, (req, res) => {
+  res.json({ success: true, user: req.user });
+});
 
 // Protected route example
 router.get('/profile', verifyToken, (req, res) => {
   res.json({
     message: 'Protected profile route',
     user: req.user
+  });
+});
+
+// Add this route
+router.get('/profile', verifyToken, (req, res) => {
+  res.json({
+    success: true,
+    user: req.user  // This comes from verifyToken middleware
   });
 });
 
